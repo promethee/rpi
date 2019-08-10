2d8900f86ae6e20d36b893982cdc0548d606f74e
lagora <lagora.franck@gmail.com>
2019-08-11 00:32:47 +0200

    add lcd 3.5" install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,3 +13,4 @@ docker volume create portainer_data
 docker pull portainer/portainer
 docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer --restart=always
 docker swarm init
+./lcd.3.5.sh

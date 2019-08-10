5c1defb42caa1d27e47f53468ed9160a7db3de64
lagora <lagora.franck@gmail.com>
2019-08-11 00:29:25 +0200

    add rust install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,11 @@ sudo apt-get upgrade -y
 sudo apt-get install -y unzip hdparm
 ./ssh.sh
 ./node.via.nvm.sh
+./rust.install.sh
 ./flash.sh
 ./hypriot.download.sh
+docker pull redis
+docker pull rabbitmq
+docker volume create portainer_data
+docker pull portainer/portainer
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer --restart=always

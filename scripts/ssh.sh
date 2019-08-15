46ab3c5007d2dae7391e43ea43f11376ca528a1c
lagora <lagora.franck@gmail.com>
2019-08-15 14:53:53 +0200

    wip

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,2 +1,5 @@
 #/bin/sh
-ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
+cd ~/
+ssh pirate@$1.local 'ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa'
+ssh pirate@$1.local 'mkdir -p .ssh'
+cat .ssh/id_rsa.pub | ssh pirate@$1.local 'cat >> .ssh/authorized_keys'
